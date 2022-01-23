d8b3357693aae75dc2dc2a40296f296269e5c9be
LummIIi <88595110+LummIIi@users.noreply.github.com>
2022-01-23 14:08:31 +0100

    add test of employee returning correct salary

---
+++ src/test/java/com/example/EmployeeTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class EmployeeTest {
+
+    Employee employee = new Employee("stina", 200);
+
+
+
+    @Test
+    void testIfEmployeeReturnsCorrectSalary() {
+        employee.setSalary(200);
+        var result = employee.getSalary();
+        assertThat(result).isEqualTo(200);
+    }
+
+
+}
+
